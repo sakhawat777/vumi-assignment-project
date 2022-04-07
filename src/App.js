834a096cbe8dfc4cc6eda02d9b0bbe1dc48be30c
sakhawat777 <biplobbms@gmail.com>
2022-04-08 01:26:34 +0600

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from './component/Login'
+import StudentList from './component/StudentList'
+import {AddStudent,EditStudent} from './component/AddStudent'
+import NotFound from './component/NotFound'
+import MyDetails from './component/MyDetails'
+import { povider, Provider } from "react-redux";
+import store from "./store";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+       <Router>
+      <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/students" component={StudentList} />
+          <Route exact path="/add-student" component={AddStudent} />
+          <Route exact path="/edit-student/:id" component={EditStudent} />
+          <Route exact path="/my-details/:id" component={MyDetails} />
+          <Route component={NotFound} />
+        </Switch>
+      {/* <h1>hii</h1> */}
+      
+      </Router>
+      </Provider>
     </div>
   );
 }
